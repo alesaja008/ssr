5ec918dc489a606549707daf1dfd3363cab51b14
Ales <alesaja008@gmail.com>
2024-03-04 13:02:46 +0700

    update siang

--- src/pages/profile/tentang-kami/index.jsx
+++ src/pages/profile/tentang-kami/index.jsx
@@ -1,5 +1,5 @@
 import MetaTentangKami from "@/components/MetaData/TentangKami";
-import SliderBlog from "@/components/elements/AllSlider/SliderBlog";
+
 import AboutBs from "@/components/statik/about";
 import HeaderAbout from "@/components/statik/about/header";
 import Head from "next/head";
@@ -10,7 +10,6 @@ export default function TentangKami() {
       <MetaTentangKami />
       <HeaderAbout />
       <AboutBs />
-      <SliderBlog />
     </>
   );
 }
